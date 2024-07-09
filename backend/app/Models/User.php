f43f30c484129b0df1fcf60ae4c022274d57ec1b
Gaoubak <gaoubak@gmail.com>
2024-07-09 15:36:26 +0200

    :sparkles: Add User and Service Model

---
+++ backend/app/Models/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Model
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'position_held',
+        'status',
+    ];
+
+    public function services(): BelongsToMany
+    {
+        return $this->belongsToMany(Service::class);
+    }
+}
